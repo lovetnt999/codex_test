0327536cbf389386ddcf99a44be312d5fbaccfa7
JeU <lovetnt999@naver.com>
2026-02-04 15:50:27 +0900

    AssemblyInfo 수정

--- SealerBeadInspectionSystem/Properties/AssemblyInfo.cs
+++ SealerBeadInspectionSystem/Properties/AssemblyInfo.cs
@@ -1,11 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Sealer Bead Inspection System")]
 [assembly: AssemblyDescription("Sample WinForms UI for a Sealer Bead Inspection System")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Example")]
-[assembly: AssemblyProduct("Sealer Bead Inspection System")]
 [assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -13,5 +9,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("a3ccbd32-9e6a-4aa9-a4e3-4c64724c04fe")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
