7fc7f661bdab0b3d8637cb0f08eab22bef3ba8e3
Jaewoo Lee <67778508+lovetnt999@users.noreply.github.com>
2026-02-04 10:44:46 +0900

    Add WinForms mock inspection UI

---
+++ SealerBeadInspectionSystem/MainForm.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+
+namespace SealerBeadInspectionSystem
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+    }
+}
